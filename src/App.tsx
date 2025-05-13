10131dddf4136ca6afe007bce65dba6937eca82a
sinopolimonopoly <sinopolicurtis@gmail.com>
2025-05-13 15:29:57 -0400

    Trying to install tailwind with 0 experience

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ export default function App(){
     return (
         <div>
             <h1>I am officially a software engineer</h1>
+            <button className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition">
+                Big ol button
+            </button>
         </div>
     );
 }
