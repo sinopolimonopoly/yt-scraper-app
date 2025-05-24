93aadeb521bdf341df0c8c3379c10ac64e439bf3
sinopolimonopoly <sinopolicurtis@gmail.com>
2025-05-24 00:21:18 -0400

    Handling invalid channel handles

--- src/scripts/apiScripts/channelIdGetter.ts
+++ src/scripts/apiScripts/channelIdGetter.ts
@@ -3,12 +3,46 @@
 // string type for parameter because this is Typescript
 // Promise<string> describes the function outputting a string
 export async function getChannelId(apiKey: string, handle: string): Promise<string> {
+
+    let handleVerified = false;
+    let validIdx;
+
+    let channelId;
+    let handleUrl;
+    let channelData;
+    let dataHandle;
+
     let url = `https://www.googleapis.com/youtube/v3/search?&type=channel&q=${handle}&key=${apiKey}`;
 
     const res = await fetch(url);
     const data = await res.json();
 
-    const channelID = data.items[0].id.channelId
 
-    return channelID
+
+    for (const [idx, item] of data.items.entries()) {
+        channelId = item.id.channelId
+        handleUrl = `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics,contentDetails&id=${channelId}&key=${apiKey}`
+        let channelRes = await fetch(handleUrl);
+        let channelData = await channelRes.json()
+        dataHandle = channelData.items[0].snippet.customUrl
+
+        console.log(idx, item);
+        console.log("current handle:", dataHandle);
+
+        if (dataHandle.toLowerCase().replace("@", "") == handle.toLowerCase()) {
+            handleVerified = true;
+            validIdx = idx;
+            break;
+        }
+    }
+
+    if (handleVerified) {
+        channelId = data.items[validIdx].id.channelId
+        return channelId
+    }
+
+    else {
+        console.log(`No channel found with handle: ${handle}.`)
+        return ""
+    }
 }
