3849a5fceef7abbbdd6e03caff539625c3fcf57f
sinopolimonopoly <sinopolicurtis@gmail.com>
2025-06-03 18:23:50 -0400

    building for deploy

--- dist/src/main.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import '@fontsource/roboto';
-import { ThemeProvider } from '@mui/material';
-import theme from './theme';
-ReactDOM.createRoot(document.getElementById('root')).render(_jsx(React.StrictMode, { children: _jsx(ThemeProvider, { theme: theme, children: _jsx(App, {}) }) }));
